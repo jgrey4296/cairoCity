b8e9f366639b73a116223269b6bd6bd63296a90f
John Grey <jgrey@ucsc.edu>
2018-06-10 21:31:04 +0100

    added an __init__.py for citygen to import all operators

---
+++ citygen/__init__.py
@@ -0,0 +1,33 @@
+#City:
+from .City import City, verify_results
+
+#operators
+from .operatorTemplate import OperatorTemplate
+from .heightmapOperator import HeightmapOperator
+from .riverOperator import RiverOperator
+from .alignOperator import AlignOperator
+from .bisectOperator import BisectOperator
+from .buildingOperator import BuildingOperator
+from .buildingSelOperator import BuildingSelOperator
+from .cloneOperator import CloneOperator
+from .crossingSelOperator import CrossingSelOperator
+from .deadendOperator import DeadendOperator
+from .destroyOperator import DestroyOperator
+from .flowOperator import FlowOperator
+from .intersectionOperator import IntersectionOperator
+from .lengthOperator import LengthOperator
+from .mergeOperator import MergeOperator
+from .moveOperator import MoveOperator
+from .plantOperator import PlantOperator
+from .pointSelOperator import PointSelOperator
+from .riverOperator import RiverOperator
+from .roadOperator import RoadOperator
+from .rotateOperator import RotateOperator
+from .spaceOperator import SpaceOperator
+from .wallOperator import WallOperator
+from .waterfillOperator import WaterfillOperator
+from .widthOperator import WidthOperator
+from .zoneCompletionOperator import ZoneCompletionOperator
+from .zoneOperator import ZoneOperator
+from .zoneSelOperator import ZoneSelOperator
+from .bezierRiverOperator import BezierRiverOperator
