ef7cd6d95cf46d970b249bf1215ec779159f0dd1
John Grey <jgrey@ucsc.edu>
2018-07-09 21:26:08 +0100

    improved main.py, splitting code out into separate functions

--- main.py
+++ main.py
@@ -30,46 +30,69 @@ INC_AMNT = 15
 SUBDIV = 10
 RADIUS = 3
 
-#The City:
-city = citygen.City()
-
 #Operators:
 heightmap = citygen.HeightmapOperator(SIZE,SUBDIV, 0, 5, 3, RADIUS)
 river = citygen.RiverOperator(100, octaves=2, deviance=0.15, repeats=None)
 bezRiv = citygen.BezierRiverOperator(1, deviance=0.15)
+roadOperator = citygen.RoadOperator([1,4], [1,4], amnt=50, hbal=0.5, hbounds=[600,1000])
+
+#The City:
+city = citygen.City(standard_rules=[],
+                    specific_rules={citygen.RoadOperator : [citygen.IntersectionOperator()]})
+
 
 #change to a stack/heap?
-all_operators = [bezRiv]
+all_operators = [roadOperator]
 
 #------------------------------
 # def MAIN TICK
 #------------------------------
 def tick(dc, i):
     if not bool(all_operators):
-        return True
+         return True
     
     #select operator
     op = city.choose_operator(all_operators)
-
+    #TODO: verify the op has an operate method
+    
     with citygen.OperatorTemplate.setup_operator(op,dc, i, city=city):
         #place operator
         
         #parameterize operator
 
         #perform operation
-        delta = op(draw=True)
+        delta = op.operate(draw=True)
 
         #verify operation
-        verify_result = city.verify(dc, delta, verify_type=type(op))
+        verify_result = city.verify(dc, i, delta, verify_type=type(op))
+            
 
         if verify_result is citygen.verify_results.FAIL:
             raise Exception("Operator failed")
 
-    #remove from pool?
-    if op.is_oneshot():
-        all_operators.remove(op)
+        #remove from pool?
+        if op.is_oneshot():
+            all_operators.remove(op)
     
     return False
+
+def save(args, theDCEL):
+    if not args.dontsave:
+        logging.info("Saving DCEL")
+        try:
+            theDCEL.savefile(dcel_filename)
+        except Exception as e:
+            logging.exception(e)
+            IPython.embed(simple_prompt=True)
+
+def draw(args, theDCEL):
+    if not args.dontdraw:
+        final_name = "{}_FINAL".format(saveString)
+        logging.info("Drawing to: {}".format(final_name))
+        utils.dcel.drawing.drawDCEL(ctx, theDCEL, faces=True, edges=True, verts=True,
+                                    background_colour=[0,0,0,1], text=True)
+        utils.drawing.write_to_png(surface, final_name)
+
 
 
 if __name__ == "__main__":
@@ -117,51 +140,47 @@ if __name__ == "__main__":
         ctx.set_source_rgba(0, 0, 0, 1)
         utils.drawing.drawRect(ctx, 0, 0, size, size)
     
-    if args.loaddcel and exists(dcel_filename):
+    if args.loaddcel and exists("{}.dcel".format(dcel_filename)):
+        logging.info("Loading DCEL")
         theDCEL = utils.dcel.DCEL.loadfile(dcel_filename)
+        IPython.embed(simple_prompt=True)
     else:
+        logging.info("Initialising DCEL")
         #make the bbox
         bbox = np.array([0,0,SIZE,SIZE])
         theDCEL = utils.dcel.DCEL(bbox=bbox)
 
         
+    #------------------------------
+    # def MAIN LOOP
+    #------------------------------
     if not args.static:
-        #------------------------------
-        # def MAIN LOOP
-        #------------------------------
-
         logging.info("Generating")
         for x in range(args.timesteps):
             logging.info("Step: {}".format(x))
             should_quit = tick(theDCEL, x)
-            
+        
             if args.drawsteps:
                 logging.info("Drawing Step: {}".format(x))
+                utils.dcel.drawing.drawDCEL(ctx, theDCEL, text=True, faces=True, edges=True, verts=True)
+                utils.drawing.write_to_png(surface, saveString, i=x)
                 if args.ipython:
                     IPython.embed(simple_prompt=True)
-                utils.dcel.drawing.drawDCEL(ctx, theDCEL, faces=True, edges=True, verts=True)
-                utils.drawing.write_to_png(surface, saveString, i=x)
-
+                
             if should_quit:
                 break
 
+        #TODO: add in a final verification at the end
+            
     #------------------------------
     # def SAVING
     #------------------------------
-    if not args.dontsave and not args.loaddcel:
-        logging.info("Saving DCEL")
-        theDCEL.savefile(dcel_filename)
-        
+    save(args, theDCEL)
 
     #------------------------------
     # def DRAWING
     #------------------------------
-    if not args.dontdraw:
-        final_name = "{}_FINAL".format(saveString)
-        logging.info("Drawing to: {}".format(final_name))
-        utils.dcel.drawing.drawDCEL(ctx, theDCEL, faces=True, edges=True, verts=True,
-                                    background_colour=[0,1,0,1])
-        utils.drawing.write_to_png(surface, final_name)
+    draw(args, theDCEL)
     
     #------------------------------
     # def INSPECTION
